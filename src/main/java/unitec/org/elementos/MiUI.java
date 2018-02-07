3063828111ebb88564a0351018e48158631df179
T-107 <T-107@PC200>
2018-02-06 21:45:57 -0600

    clase 6/2/18

--- src/main/java/unitec/org/elementos/MiUI.java
+++ src/main/java/unitec/org/elementos/MiUI.java
@@ -9,16 +9,26 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 @Theme ("valo")
 public class MiUI extends UI{
+    public TextField t1;
+    public TextField t2;
+    public Integer id;
+    @Autowired
+    RepositorioMensajitos repoMensa;
 
     @Override
     protected void init(VaadinRequest request) {
@@ -28,13 +38,14 @@ public class MiUI extends UI{
      Label e1=new Label("Carlos Eduardo");
      e1.addStyleName(ValoTheme.LABEL_H1);
      
-     Button b1=new Button("click aqui");
+     Button b1=new Button("Guardar");
      b1.addStyleName(ValoTheme.BUTTON_HUGE);
      
      //Vamos a programar el evento del boton b1 usando programacion funcional 
      b1.addClickListener(algo->{
         //aqui vamos a poner el evento
-        e1.setValue("Mi primer programacion funcional");
+        //e1.setValue("Mi primer programacion funcional");
+        Notification.show("Error", Notification.Type.ERROR_MESSAGE);
      });
      
      //agregamos las componentes al layout
@@ -46,18 +57,48 @@ public class MiUI extends UI{
      setContent(layout);
      
      // Have some data
-List<Mensajito> mensaje = Arrays.asList(
-    new Mensajito("Nicolaus Copernicus", 1543),
-    new Mensajito("Galileo Galilei", 1564),
-    new Mensajito("Johannes Kepler", 1571));
+List<Mensajitos> mensaje = (List<Mensajitos>) repoMensa.findAll();
 
 // Create a grid bound to the list
-Grid<Mensajito> grid = new Grid<>();
+Grid<Mensajitos> grid = new Grid<>();
 grid.setItems(mensaje);
-grid.addColumn(Mensajito::getTitulo).setCaption("Name");
-grid.addColumn(Person::getCuerpo).setCaption("Year of birth");
+grid.addColumn(Mensajitos::getId).setCaption("ID");
+grid.addColumn(Mensajitos::getTitulo).setCaption("Titulo");
+grid.addColumn(Mensajitos::getCuerpo).setCaption("Cuerpo del mensaje");
+
+grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+
+grid.addItemClickListener(evento -> {
+    Notification.show("Valor: "+ evento.getItem().getTitulo());
+    Ventana sub = new Ventana();
+    UI.getCurrent().addWindow(sub);
+    
+});
 
 layout.addComponent(grid);
     }
     
 }
+
+class Ventana extends Window {
+    public Ventana(){
+        super("actualizar o borrar");
+        center();
+        VerticalLayout v12 =new VerticalLayout();
+        
+        //TextField t1=new TextField();
+        //TextField t2=new TextField();
+        
+        
+        Button boton =new Button ("actualizar");
+        boton.addClickListener(evento -> {
+        close();
+        });
+        v12.addComponent(t1);
+        v12.addComponent(t2);
+        v12.addComponent(boton);
+        
+        setContent(v12);
+    }
+    }
+
